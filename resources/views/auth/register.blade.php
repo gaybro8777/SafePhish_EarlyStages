da46429dc6dde9aef7cfb901cbb5457b64b57375
Throckmorton <tylerthrock95@gmail.com>
2016-07-29 15:41:01 -0400

    Changed name: regTest > register; updated form to include email

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,65 +1,22 @@
-@extends('app')
-
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+<html>
+<head>
+    <title>Generate Emails Form</title>
+    <meta name="_token" content="{{ csrf_token() }}" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+</head>
+<body>
+<div class="rootContainer">{!! Form::open(array('action'=>'PhishingController@postRegister')) !!}
+    {!! Form::text('firstNameText',null,array('name'=>'firstNameText')) !!}
+    {!! Form::text('lastNameText',null,array('name'=>'lastNameText')) !!}
+    {!! Form::text('initialText',null,array('name'=>'initialText','maxlength'=>1)) !!}
+    {!! Form::text('emailText',null,array('name'=>'emailText')) !!}
+    {!! Form::text('usernameText',null,array('name'=>'usernameText')) !!}
+    {!! Form::password('passwordText',array('name'=>'passwordText')) !!}
+    {!! Form::submit('Submit',array('id'=>'submitButton')) !!}
+{!! Form::close() !!}
 </div>
-@endsection
+<footer>
+    <p></p>
+</footer>
+</body>
+</html>
